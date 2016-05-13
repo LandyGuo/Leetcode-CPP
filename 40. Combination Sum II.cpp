9a88f06430a38018f2089ae74dc09b73ee4ffc49
LandyGuo <gqp.hust@gmail.com>
2016-05-13 19:56:32 +0800

    Update 40. Combination Sum II.cpp

--- 40. Combination Sum II.cpp
+++ 40. Combination Sum II.cpp
@@ -17,39 +17,36 @@ A solution set is:
 [1, 1, 6] 
 */
 /*2nd-pass
-思路：
-多个数的组合和，和固定的2-sum，3-sum问题有本质区别，用dfs遍历所有可能的情形
+思路:与39题完全相同，每一层都不能有相同的节点
 */
-
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vector<vector<int>>res;
-        vector<int>path;
-        dfs(candidates.begin(),candidates.end(),target,path,res);
-        return res;
-    }
-    
-    
-    void dfs(vector<int>::iterator p1,vector<int>::iterator p2,int target,vector<int>&path,vector<vector<int>>&res)
-    {
-        if(target==0)
-        {
-            if(find(res.begin(),res.end(),path)==res.end())
-            {
-                res.push_back(path);
-            }
-            return;
-        }
-        for(auto p= p1;p!=p2;p++)
-        {
-            if(*p>target)
-                continue;
-            path.push_back(*p);
-            dfs(p+1,p2,target-*p,path,res);
-            path.pop_back();
-        }
-        
-    }
+	vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+		vector<vector<int>>ret;
+		if (candidates.empty() || target <= 0) return ret;
+		sort(candidates.begin(), candidates.end());
+		vector<int> path;
+        dfs(candidates,-1,target,path,ret);
+		return ret;
+	}
+
+	void dfs(vector<int>& candidates,int cur_index,int cur_target, vector<int>&path, vector<vector<int>>&res)
+	{
+		if (cur_target == 0)
+			res.push_back(path);
+		else if (cur_target<0)
+			return;
+		else
+		{
+			for (int i=cur_index+1;i<candidates.size();i++)
+			{
+				if (candidates[i]>cur_target)
+					break;
+				if (i != cur_index+1 && candidates[i] == candidates[i - 1]) continue;
+				path.push_back(candidates[i]);
+				dfs(candidates,i, cur_target - candidates[i], path, res);
+				path.pop_back();
+			}
+		}
+	}
 };
